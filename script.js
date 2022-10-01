d6f25e2332f206bf08be4116c32635735162b130
mMartinez00 <martinez.mauricio.0093@gmail.com>
2022-09-30 21:56:31 -0400

    throttle logic implemented to nav shadow

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
+const nav = document.getElementById("nav");
+const nav__links = document.querySelectorAll(".nav__link");
+let throttlePause;
 
-document.querySelector('.about__')
+function throttle(callback, delay) {
+  if (throttlePause) return;
+
+  throttlePause = true;
+
+  setTimeout(() => {
+    callback();
+
+    throttlePause = false;
+  }, delay);
+}
+
+function scroll() {
+  if (window.scrollY === 0) {
+    nav.classList.remove("nav--shadow");
+  }
+
+  if (window.scrollY > 20) {
+    nav.classList.add("nav--shadow");
+  }
+
+  if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+    nav.classList.remove("nav--shadow");
+  }
+}
+
+window.addEventListener("scroll", () => {
+  throttle(scroll, 250);
+});
