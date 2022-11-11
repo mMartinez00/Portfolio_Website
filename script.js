2f1f09d6d1a48801c8cf3aa03813e19f83609aaf
mMartinez00 <martinez.mauricio.0093@gmail.com>
2022-11-10 19:46:57 -0500

    function a and b created to change the nav__link border on scroll and click

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const nav = document.getElementById("nav");
 const nav__links = document.querySelectorAll(".nav__link");
+const sections = document.querySelectorAll("#about, #projects, #contact");
 let throttlePause;
 
 function throttle(callback, delay) {
@@ -26,7 +27,41 @@ function scroll() {
   if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
     nav.classList.remove("nav--shadow");
   }
+
+  b();
 }
+
+function a() {
+  let current = document.querySelector(".nav__link--active");
+
+  current.className = "nav__link";
+
+  this.classList.add("nav__link--active");
+}
+
+function b() {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  nav__links.forEach((link) => {
+    link.classList.remove("nav__link--active");
+
+    if (link.innerText.toLowerCase() === current) {
+      link.classList.add("nav__link--active");
+    }
+  });
+}
+
+nav__links.forEach((link) => {
+  link.addEventListener("click", a);
+});
 
 window.addEventListener("scroll", () => {
   throttle(scroll, 250);
